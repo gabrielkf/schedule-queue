ce9bef89850389d15c0f624c26634ec3bfeae047
gabrielkf <gabrielkf@gmail.com>
2022-06-22 19:07:11 -0300

    Tweet counting service with interval time

--- src/tweets/tweet-count/tweet-count.service.ts
+++ src/tweets/tweet-count/tweet-count.service.ts
@@ -1,14 +1,40 @@
-import { Injectable } from "@nestjs/common";
+import { CACHE_MANAGER, Inject, Injectable } from "@nestjs/common";
 import { Interval } from "@nestjs/schedule";
 import { InjectModel } from "@nestjs/sequelize";
 import { Tweet } from "../entities/tweet.entity";
+import { Cache } from "cache-manager";
+
+const CHECK_INTERVAL = 5000;
 
 @Injectable()
 export class TweetCountService {
-  constructor(@InjectModel(Tweet) private tweetModel: typeof Tweet) {}
+  private OFFSET_CACHE_KEY = "tweet-offset";
+  private OFFSET_INITIAL_VALUE = 0;
+  private COUNT_LIMIT = 10;
 
-  @Interval(5000)
+  constructor(
+    @InjectModel(Tweet) private tweetModel: typeof Tweet,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
+
+  @Interval(CHECK_INTERVAL)
   async countTweets() {
-    console.log("counting");
+    const offset =
+      (await this.cacheManager.get<number>(this.OFFSET_CACHE_KEY)) ??
+      this.OFFSET_INITIAL_VALUE;
+
+    const tweets = await this.tweetModel.findAll({
+      offset,
+      limit: this.COUNT_LIMIT,
+    });
+
+    if (tweets.length >= this.COUNT_LIMIT) {
+      await this.cacheManager.set(
+        this.OFFSET_CACHE_KEY,
+        offset + this.COUNT_LIMIT,
+      );
+
+      console.log(`Increasing offset to ${offset + this.COUNT_LIMIT}`);
+    }
   }
 }
--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -1,12 +1,17 @@
 import { Tweet } from "./entities/tweet.entity";
 import { SequelizeModule } from "@nestjs/sequelize";
-import { Module } from "@nestjs/common";
+import { CacheModule, Module } from "@nestjs/common";
 import { TweetsService } from "./tweets.service";
 import { TweetsController } from "./tweets.controller";
 import { TweetCountService } from "./tweet-count/tweet-count.service";
 
+const DEFAULT_TIME_TO_LIVE = 60 * 60;
+
 @Module({
-  imports: [SequelizeModule.forFeature([Tweet])],
+  imports: [
+    CacheModule.register({ ttl: DEFAULT_TIME_TO_LIVE }),
+    SequelizeModule.forFeature([Tweet]),
+  ],
   controllers: [TweetsController],
   providers: [TweetsService, TweetCountService],
 })
